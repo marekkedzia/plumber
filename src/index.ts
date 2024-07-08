a04169d96e2ec48e21cd1ec3be7fcb409caca3b6
Marek Kędzia <marekkedzia>
2024-07-08 18:41:03 +0200

    add export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {PlumberRouter} from "./router";
 export {RouterConfig, RouterPath} from "./interfaces/router.config";
 export {Pipe, Filter} from "./interfaces/router.middleware";
+export {ParameterizedRequest} from "./interfaces/parameterized.request";
