cc114403791d0d40000c907fccd57baff4b03b0d
Marek Kędzia <marekkedzia>
2024-07-08 18:37:43 +0200

    add filter and pipe constructor

--- src/interfaces/router.middleware.ts
+++ src/interfaces/router.middleware.ts
@@ -9,6 +9,21 @@ type Filter<T> = {
     type: "filter";
     handle: (request: ParameterizedRequest<T>) => T | Promise<T>;
 }
+
+function Filter<T>(handle: (request: ParameterizedRequest<T>) => T | Promise<T>): Filter<T> {
+    return {
+        type: "filter",
+        handle
+    };
+}
+
+function Pipe<T>(handle: (request: ParameterizedRequest<T>) => void | Promise<void>): Pipe<T> {
+    return {
+        type: "pipe",
+        handle
+    };
+}
+
 type RouterMiddleware<T> = Pipe<T> | Filter<T>;
 
 export {RouterMiddleware, Pipe, Filter};
